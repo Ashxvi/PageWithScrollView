78e591381a53b25fe7696477ec5b5cd0babe17e9
Achraf Mahha <mahha@mbp-de-mahha.boiscolombes.fr.ibm.com>
2017-03-21 11:28:58 +0100

    First commit

--- PageWithScrollView/ViewController.swift
+++ PageWithScrollView/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        var contentWidth : CGFloat = 0.0
+        let scrollwidth : CGFloat = scrollView.frame.size.width
+    
+        for i in 0...2 {
+            
+            let icon = UIImage(named: "icon\(i).png")
+            let imageView = UIImageView(image : icon)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollwidth/2 + (scrollwidth)*CGFloat(i)
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX-75, y: view.frame.midY-75, width: 150, height: 150) // 75 = 150/2
+            
+            
+            
+           contentWidth += newX
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
+        
     }
 
     override func didReceiveMemoryWarning() {
